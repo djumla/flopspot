024b0cd381d8005205d5c297408d5e7fb1233d4b
Sven Ahrens <svenahrens@Svens-MBP.fritz.box>
2017-09-07 12:09:07 +0200

    New less folder structure, progress on design

--- gulpfile.js
+++ gulpfile.js
@@ -1,9 +1,19 @@
 var gulp = require('gulp'),
   less = require('gulp-less'),
-  livereload = require('gulp-livereload');
+  livereload = require('gulp-livereload'),
+  plumber = require('gulp-plumber'),
+  gutil = require('gulp-util');
+
+var onError = function(err) {
+  gutil.beep();
+  console.log(err);
+};
 
 gulp.task('less', function() {
   return gulp.src('./public/*.less')
+    .pipe(plumber({
+      errorHandler: onError
+    }))
     .pipe(less())
     .pipe(gulp.dest('./dist/'))
     .pipe(livereload());
