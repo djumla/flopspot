270dde9fd8f4c81b25c12b3c8d258889850dd865
Sven Ahrens <svenahrens@Svens-MBP.fritz.box>
2017-10-24 14:59:20 +0200

    Added contact, imprint, privacy blade files

---
+++ resources/views/layouts/privacyPolicy.blade.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>flopspot</title>
+    <meta name="description" content="Description">
+    <meta name="author" content="Sven Ahrens">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <link href="https://fonts.googleapis.com/css?family=Noto+Sans|Noto+Serif|Open+Sans|Poppins" rel="stylesheet">
+
+    <link rel="stylesheet" href="/css/app.css" >
+
+    <script
+    src="https://code.jquery.com/jquery-3.2.1.js"
+    integrity="sha256-DZAnKJ/6XZ9si04Hgrsxu/8s717jcIzLy3oi35EouyE="
+    crossorigin="anonymous"
+    ></script>
+    <script src="./js/flopspot-responsive.js"></script>
+  </head>
+  <body>
+    <div id="app">
+    </div>
+  </body>
+</html>
