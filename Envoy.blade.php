919ac3f7c55afdfbaf0afedd198b2450367288e9
Sven Ahrens <svenahrens@Svens-MacBook-Pro.local>
2017-11-12 16:50:24 +0100

    Revert "delete envoy"
    This reverts commit f807360a4ed688d5fd7c2d2e94c642cb76c43dd1.

---
+++ Envoy.blade.php
@@ -0,0 +1,27 @@
+@servers(["live" => "app@flopspot.de -p 222"])
+
+@task("deploy-dev", ["on" => "live"])
+cd /var/www/flopspot.de/dev.flopspot.de
+git pull
+composer install
+php artisan migrate
+@endtask
+
+@task('parse-dev', ['on' => 'live'])
+cd /var/www/flopspot.de/dev.flopspot.de
+php artisan migrate
+php artisan xml:parse
+@endtask
+
+@task("deploy-live", ["on" => "live"])
+cd /var/www/flopspot.de/flopspot.de
+git pull
+composer install
+php artisan migrate
+@endtask
+
+@task('parse-live', ['on' => 'live'])
+cd /var/www/flopspot.de/flopspot.de
+php artisan migrate
+php artisan xml:parse
+@endtask
