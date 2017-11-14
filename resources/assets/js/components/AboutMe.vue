2a412dc39cfd37a1c92b062f80953603b6f9ac11
Sven Ahrens <svenahrens@Svens-MBP.fritz.box>
2017-11-14 17:29:00 +0100

    Updated about me text

--- resources/assets/js/components/AboutMe.vue
+++ resources/assets/js/components/AboutMe.vue
@@ -5,8 +5,8 @@
         <h1 class="underlined">Was ist flopspot?</h1>
         <h2>Flopspot ist das folgende..</h2>
         <p>
-          flopspot ist eine Webapplikation zur Erfassung der Qualität der WLAN Hotspots in den Zügen der Deutschen Bahn. Durch die erfassten Daten der Benutzer werden Schaubilder generiert, um die Zuverlässigkeit des Hotspots der Bahn darzustellen. flopspot wurde
-          innerhalb eines Schulprojektes in Kooperation mit der Firma djumla entwickelt. Das Projekt ist Open Source und auf GitHub zu finden: www.github.com
+          flopspot ist eine Webapplikation zur Erfassung der Qualität der WLAN Hotspots in den Zügen der Deutschen Bahn. Durch die erfassten Daten der Benutzer werden Statistiken generiert, um die Zuverlässigkeit des Hotspots der Bahn darzustellen. flopspot wurde
+          von der Firma <a href="https://www.djumla.de">djumla webentwicklung</a> entwickelt. Das Projekt ist Open Source und auf <a href="github.com/djumla/flopspot">GitHub</a> zu finden.
         </p>
       </div>
     </section>
