b6d18c27c836d20fe6a1d99e600b51dc7f5e7fd5
Sven Ahrens <svenahrens@Svens-MBP.fritz.box>
2017-11-14 16:11:44 +0100

    Minified jQuery

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -18,10 +18,11 @@
   </div>
   <script src="{{ asset('js/app.js') }}"></script>
   <script
-          src="https://code.jquery.com/jquery-3.2.1.js"
-          integrity="sha256-DZAnKJ/6XZ9si04Hgrsxu/8s717jcIzLy3oi35EouyE="
-          crossorigin="anonymous"
-  ></script>
+          src="https://code.jquery.com/jquery-3.2.1.min.js"
+          integrity="sha256-hwg4gsxgFZhOsEEamdOYGBf13FyQuiTwlAQgxVSNgt4="
+          crossorigin="anonymous">
+
+  </script>
   <script src="js/responsive.js"></script>
   <script src="js/observer.js"></script>
 @endsection
