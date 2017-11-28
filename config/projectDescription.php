74d8c31317ad2e74eb961ac36a2a5f5c8318761a
Sven Ahrens <svenahrens@Svens-MBP.fritz.box>
2017-11-28 17:32:16 +0100

    Updated flopspot text in projectDescription

--- config/projectDescription.php
+++ config/projectDescription.php
@@ -2,6 +2,6 @@
 
 return [
 
-    'description' => 'Hey, Du! Hast du Erfahrungen mit dem WLAN der DB, die du mit uns teilen willst? Auf https://www.flopspot.de/ kannst du das Internet auf deiner Zugfahrt bewerten und hilfst uns dadurch, die Qualtität des Bahn-WLAN zu erfassen.'
+    'description' => 'Hey, Du! Hast du Erfahrungen mit dem WLAN der DB, die du mit uns teilen willst? Auf https://www.flopspot.de/ kannst du das Internet auf deiner Zugfahrt bewerten und hilfst uns dadurch, die Qualität des Bahn-WLAN zu erfassen.'
 
 ];
